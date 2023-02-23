5ed7e44fffbb6557003fce0be9fc86c119bd0187
Daniel Liljeström <daniel@mailbox.sh>
2023-02-23 19:03:50 +0100

    nvim: remove friendly snippets

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -2,12 +2,6 @@ return {
 	-- snippets
 	{
 		'L3MON4D3/LuaSnip',
-		dependencies = {
-			'rafamadriz/friendly-snippets',
-			config = function()
-				require('luasnip.loaders.from_vscode').lazy_load()
-			end,
-		},
 		opts = {
 			history = true,
 			delete_check_events = 'TextChanged',
