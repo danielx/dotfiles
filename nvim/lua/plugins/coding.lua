5eedc8eda56ec4027614393cec097053dcf6c8e8
Daniel Liljeström <daniel@mailbox.sh>
2023-03-05 17:17:15 +0100

    nvim: autocomplete keybinds

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -45,9 +45,9 @@ return {
 				mapping = cmp.mapping.preset.insert({
 						["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 						["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-						["<C-b>"] = cmp.mapping.scroll_docs(-4),
-						["<C-f>"] = cmp.mapping.scroll_docs(4),
-						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-u>"] = cmp.mapping.scroll_docs(-4),
+						["<C-d>"] = cmp.mapping.scroll_docs(4),
+						["<C-f>"] = cmp.mapping.complete(),
 						["<C-e>"] = cmp.mapping.abort(),
 						["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 						["<S-CR>"] = cmp.mapping.confirm({
