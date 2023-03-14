79fbe50efc4106fbb82c18174281abf2be436a32
Daniel Liljeström <daniel@mailbox.sh>
2023-03-14 20:30:36 +0100

    nvim: better golang integration

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -170,4 +170,20 @@ return {
 			require('mini.splitjoin').setup(opts)
 		end
 	},
+
+	-- Golang development
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			-- "ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", 'gomod' },
+		build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+	}
 }
