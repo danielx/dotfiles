32e1ca613f5dbd3950a1b85b5242b331f9e2e8e9
Daniel Liljeström <daniel@mailbox.sh>
2023-03-23 22:45:52 +0100

    nvim: c-f to accept auto completions

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -46,13 +46,9 @@ return {
 						["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 						["<C-u>"] = cmp.mapping.scroll_docs(-4),
 						["<C-d>"] = cmp.mapping.scroll_docs(4),
-						["<C-f>"] = cmp.mapping.complete(),
+						["<C-s>"] = cmp.mapping.complete(),
 						["<C-e>"] = cmp.mapping.abort(),
-						["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-						["<S-CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+						["<C-f>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
