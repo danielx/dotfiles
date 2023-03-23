12ed9940ca88cbcf0a284cee568ac3337408168e
Daniel Liljeström <daniel@mailbox.sh>
2023-03-23 18:57:24 +0100

    nvim: do not sync on build

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -184,6 +184,5 @@ return {
 		end,
 		event = { "CmdlineEnter" },
 		ft = { "go", 'gomod' },
-		build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
 	}
 }
