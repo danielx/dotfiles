47b22fd11de8d6269f6f2cac2b3e4f33b457fd7a
Daniel Liljeström <daniel@mailbox.sh>
2023-04-13 15:24:21 +0200

    nvim(fterm): rounded borders

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -158,7 +158,10 @@ return {
         keys = {
             { '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>' },
             { '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', mode = { 't' } }
-        }
+        },
+        opts = {
+            border = 'rounded',
+        },
     },
 
     -- help :)
