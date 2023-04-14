13932e31e5fa02282ce558f444465f27b5b183ce
Daniel Liljeström <daniel@mailbox.sh>
2023-04-14 19:05:23 +0200

    nvim(luasnip): disable tab jumps on mode change

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -20,6 +20,15 @@ return {
 			{ '<tab>',   function() require('luasnip').jump(1) end,  mode = 's' },
 			{ '<s-tab>', function() require('luasnip').jump(-1) end, mode = { 'i', 's' } },
 		},
+		config = function()
+			vim.api.nvim_create_autocmd('InsertLeave', {
+				callback = function()
+					if require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()] then
+						require("luasnip").unlink_current()
+					end
+				end
+			})
+		end,
 	},
 
 	-- auto completion
