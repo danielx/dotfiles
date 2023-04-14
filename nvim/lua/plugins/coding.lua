7693d4194ed84fea8335b3ac9058498b9a087e40
Daniel Liljeström <daniel@mailbox.sh>
2023-04-14 19:06:25 +0200

    nvim: lua fmt

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -51,14 +51,14 @@ return {
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-						["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-						["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-						["<C-u>"] = cmp.mapping.scroll_docs(-4),
-						["<C-d>"] = cmp.mapping.scroll_docs(4),
-						["<C-f>"] = cmp.mapping.complete(),
-						["<C-e>"] = cmp.mapping.abort(),
-						["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-						["<S-CR>"] = cmp.mapping.confirm({
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-f>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<S-CR>"] = cmp.mapping.confirm({
 						behavior = cmp.ConfirmBehavior.Replace,
 						select = true,
 					}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
