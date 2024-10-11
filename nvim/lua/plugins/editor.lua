d525deafb4433f67bac9a4bc243465a8c3206d27
Daniel Liljeström <daniel@mailbox.sh>
2024-10-11 09:52:05 +0200

    nvim(which-key): fix breaking change

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -168,31 +168,16 @@ return {
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
-		opts = {
-			window = {
-				border = "single", -- none, single, double, shadow
-				position = "bottom", -- bottom, top
-				margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-				padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-				winblend = 0,
-			},
-			layout = {
-				height = { min = 4, max = 25 }, -- min and max height of the columns
-				width = { min = 20, max = 50 }, -- min and max width of the columns
-				spacing = 3, -- spacing between columns
-				align = "left", -- align columns left, center or right
-			},
-		},
 		config = function(_, opts)
 			local wk = require("which-key")
 			wk.setup(opts)
-			wk.register({
-				["<leader>s"] = { name = "+search" },
-				["<leader>l"] = { name = "+lsp" },
-				["<leader>c"] = { name = "+lsp" },
-				["<leader>w"] = { name = "+workspace" },
-				["<leader>x"] = { name = "+trouble" },
-				["<leader>g"] = { name = "+git" },
+			wk.add({
+				{ "<leader>s", group = "+search" },
+				{ "<leader>l", group = "+lsp" },
+				{ "<leader>c", group = "+lsp" },
+				{ "<leader>w", group = "+workspace" },
+				{ "<leader>x", group = "+trouble" },
+				{ "<leader>g", group = "+git" },
 			})
 		end,
 	},
@@ -304,10 +289,10 @@ return {
 		"linrongbin16/gitlinker.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
-			require('gitlinker').setup({
+			require("gitlinker").setup({
 				router = {
 					browse = {
-						["^github%..*"] = require('gitlinker.routers').github_browse,
+						["^github%..*"] = require("gitlinker.routers").github_browse,
 					},
 				},
 			})
